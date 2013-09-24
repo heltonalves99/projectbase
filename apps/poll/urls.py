35c9b42297d9ea0daf3a78287e1960969acd9b1a
Helton Alves <helton99@hotmail.com>
2013-09-24 23:20:04 +0000

    change generic view vote

--- apps/poll/urls.py
+++ apps/poll/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, url
 
 from .views import QuestionListView
 from .views import QuestionDetailView
+from .views import QuestionRedirectView
 
 urlpatterns = patterns('',
     url(r'^$', QuestionListView.as_view(), name='question_list'),
     url(r'^(?P<pk>\d+)/$', QuestionDetailView.as_view(), name='question_detail'),
+    url(r'^(?P<pk>\d+)/vote/$', QuestionRedirectView.as_view(), name='question_vote'),
 )
--- apps/poll/views.py
+++ apps/poll/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
 
 from .models import Question
+from .models import Alternative
 
 
 class QuestionListView(ListView):
@@ -11,4 +15,15 @@ class QuestionListView(ListView):
 
 class QuestionDetailView(DetailView):
     model = Question
+
+class QuestionRedirectView(RedirectView):
+    def post(self, request, *args, **kwargs):
+        alternative = Alternative.objects.get(id=request.POST['alternative'])
+        alternative.vote += 1
+        alternative.save()
+        messages.success(request, u"Seu voto foi enviado com sucesso.")
+        return super(QuestionRedirectView, self).post(request, *args, **kwargs)
+
+    def get_redirect_url(self, **kwargs):
+        return reverse_lazy('poll:question_detail', args=[kwargs['pk']])
 
